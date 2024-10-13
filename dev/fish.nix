9cfa41faedf2334c17d71bd7cc4a174b27bcb8db
Zap <67962871+ziap@users.noreply.github.com>
2024-10-13 19:40:42 +0700

    Update fish configuration

--- dev/fish.nix
+++ dev/fish.nix
@@ -9,6 +9,11 @@
     eza fd ripgrep bat skim
   ];
 
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+
+
   programs.fish = {
     enable = true;
 
@@ -37,10 +42,11 @@
         fish_default_key_bindings -M insert
         fish_vi_key_bindings --no-erase
       end
-      
+
       set -g fish_key_bindings fish_hybrid_key_bindings
       bind -M insert -k nul accept-autosuggestion
-      
+      bind -M insert \cF edit_command_buffer
+
       set fish_cursor_default block
       set fish_cursor_insert line
       set fish_cursor_replace_one underscore
