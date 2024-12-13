e38a8559d5ad7552ded4d079887f00cce5cae516
Zap <67962871+ziap@users.noreply.github.com>
2024-12-13 12:17:39 +0700

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -12,19 +12,15 @@
 
   outputs = { nixpkgs, home-manager, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
-      defaultPackage.${system} = home-manager.defaultPackage.${system};
+      # Set home manager as the default package so that we can `nix run .`
+      defaultPackage = home-manager.defaultPackage;
+
+      # Home manager configuration modules
       homeConfigurations."zap" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
         modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
     };
 }
