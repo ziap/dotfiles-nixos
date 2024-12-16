dee4c71d89c557309a6872d6572a67ab8e17cffd
Zap <67962871+ziap@users.noreply.github.com>
2024-12-16 17:03:49 +0700

    Add desktop integration portal

--- user.nix
+++ user.nix
@@ -66,4 +66,10 @@ in {
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
   };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  };
 }
