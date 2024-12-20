8dc301f50998e0e08afbe7a82e364ab5e6173269
Zap <67962871+ziap@users.noreply.github.com>
2024-12-20 20:47:44 +0700

    Update nvim-cmp configuration

--- dev/nvim/plugins/cmp.lua
+++ dev/nvim/plugins/cmp.lua
@@ -2,7 +2,6 @@ local cmp = require'cmp'
 
 cmp.setup({
   snippet = {
-    -- REQUIRED - you must specify a snippet engine
     expand = function(args)
       vim.fn['vsnip#anonymous'](args.body)
     end
@@ -10,7 +9,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<c-b>'] = cmp.mapping.scroll_docs(-4),
     ['<c-f>'] = cmp.mapping.scroll_docs(4),
-    ['<c-space>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<c-space>'] = cmp.mapping.confirm({ select = true }),
     ['<c-e>'] = cmp.mapping.abort()
   }),
   sources = cmp.config.sources({
@@ -21,24 +20,13 @@ cmp.setup({
   })
 })
 
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' }
-  })
-})
-
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline({'/', '?'}, {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = 'buffer' }
   }
 })
 
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
