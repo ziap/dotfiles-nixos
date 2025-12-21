c9d43486451d361ccfad90dbd8c569f5b3129477
Zap <67962871+ziap@users.noreply.github.com>
2025-12-21 18:46:13 +0700

    update flake

--- dev/git.nix
+++ dev/git.nix
@@ -4,27 +4,28 @@
   programs.git = {
     enable = true;
 
-    userName = "Zap";
-    userEmail = "67962871+ziap@users.noreply.github.com";
-
-    delta = {
-      enable = true;
-      options = {
-        line-numbers = true;
-        syntax-theme = "gruvbox-dark";
-        side-by-side = false;
-        file-modified-label = "modified:";
+    settings = {
+      user = {
+        name = "Zap";
+        email = "67962871+ziap@users.noreply.github.com";
       };
-    };
-
-    extraConfig = {
-      merge.tool = "nvimdiff3";
     };
   };
 
   programs.gh = {
     enable = true;
     gitCredentialHelper.enable = true;
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      line-numbers = true;
+      syntax-theme = "gruvbox-dark";
+      side-by-side = false;
+      file-modified-label = "modified:";
+    };
   };
 }
 
